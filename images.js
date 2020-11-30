d6838e7fcea22da00b98b0e86bd89e5120976338
Noble <noblereign@users.noreply.github.com>
2020-11-30 09:19:43 -0800

    attempt one billion
    wew

--- images.js
+++ images.js
@@ -1,42 +1,79 @@
+let loadAmt = 0;
+
 function loadImages() {
 	const xhttp = new XMLHttpRequest();
 
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			const fileList = this.responseText.split('\n'); // Split by lines
-			let currentFolder = '';
+			if (loadAmt == 1) {
+				const fileList = this.responseText.split('\n'); // Split by lines
+				let message = '';
+				let artistName = '';
+				
+				const filePaths = fileList
+					.map(f => { // Build correct path for each file
 
-			const filePaths = fileList
-				.map(f => { // Build correct path for each file
-					let filePath = '';
-
-					if (f) {
-						if (f[0] === '.') {
-							currentFolder = f.replace('.', '').replace(':', '/');
+						if (f) {
+							if (f[0] === '.') {
+								var strs = f.split('{');
+								message = strs[1];
+								artistName = strs[0];
+							}
 						}
-						else if (f[f.length - 1] !== '/') {
-							filePath = `${location.href}${currentFolder}${f}`;
-						}
-					}
+			  
+						return message;
+					})
+					.filter(f => f); // Remove empty lines
 		  
-					return filePath;
-				})
-				.filter(f => f); // Remove empty lines
-	  
-			const imagesContainer = document.getElementById('glaciers');
-	  
-			filePaths.map(f => { // Create and put images to the DOM
-				const img = document.createElement('IMG');
-				img.setAttribute('class', "materialboxed");
-				img.setAttribute('data-caption', "test caption uwu");
-				img.setAttribute('width', "350");
-				img.src = f;
-				imagesContainer.appendChild(img);
-			});
+				const images = document.querySelectorAll('[artist="' + artistName + '"]');
+		  
+				images.forEach(function (arrayItem) {
+					arrayItem.setAttribute('data-caption', message);
+				});
+			}
+			else
+			{
+				const fileList = this.responseText.split('\n'); // Split by lines
+				let currentFolder = '';
+				let artistName = '';
+				
+				const filePaths = fileList
+					.map(f => { // Build correct path for each file
+						let filePath = '';
+
+						if (f) {
+							if (f[0] === '.') {
+								currentFolder = f.replace('.', '').replace(':', '/');
+								artistName = f.replace('imgs', '').replace('/','');
+							}
+							else if (f[f.length - 1] !== '/') {
+								filePath = `${location.href}${currentFolder}${f}`;
+							}
+						}
+			  
+						return filePath;
+					})
+					.filter(f => f); // Remove empty lines
+		  
+				const imagesContainer = document.getElementById('glaciers');
+		  
+				filePaths.map(f => { // Create and put images to the DOM
+					const img = document.createElement('IMG');
+					img.setAttribute('class', "materialboxed");
+					img.setAttribute('data-caption', "Has anybody ever told you how cute your generic, nondescript features are?");
+					img.setAttribute('width', "350");
+					img.setAttribute('artist', artistName);
+					img.src = f;
+					imagesContainer.appendChild(img);
+				});
+			}
 		}
 	};
 
 	xhttp.open("GET", "images.txt", true);
+	xhttp.send();
+	loadAmt++;
+	xhttp.open("GET", "descs.txt", true);
 	xhttp.send();
 }
 
