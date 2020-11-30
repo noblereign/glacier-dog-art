75163a7414f52bcd0fe2b290b5c3c5f890dda062
Noble <noblereign@users.noreply.github.com>
2020-11-30 09:27:42 -0800

    Update images.js

--- images.js
+++ images.js
@@ -1,80 +1,83 @@
-let loadAmt = 0;
-
 function loadImages() {
 	const xhttp = new XMLHttpRequest();
 
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			if (loadAmt == 1) {
-				const fileList = this.responseText.split('\n'); // Split by lines
-				let message = '';
-				let artistName = '';
-				
-				const filePaths = fileList
-					.map(f => { // Build correct path for each file
+			const fileList = this.responseText.split('\n'); // Split by lines
+			let currentFolder = '';
+			let artistName = '';
+			
+			const filePaths = fileList
+			.map(f => { // Build correct path for each file
+				let filePath = '';
 
-						if (f) {
-							if (f[0] === '.') {
-								var strs = f.split('{');
-								message = strs[1];
-								artistName = strs[0];
-							}
-						}
-			  
-						return message;
-					})
-					.filter(f => f); // Remove empty lines
-		  
-				const images = document.querySelectorAll('[artist="' + artistName + '"]');
-		  
-				images.forEach(function (arrayItem) {
-					arrayItem.setAttribute('data-caption', message);
-				});
-			}
-			else
-			{
-				const fileList = this.responseText.split('\n'); // Split by lines
-				let currentFolder = '';
-				let artistName = '';
-				
-				const filePaths = fileList
-					.map(f => { // Build correct path for each file
-						let filePath = '';
-
-						if (f) {
-							if (f[0] === '.') {
-								currentFolder = f.replace('.', '').replace(':', '/');
-								artistName = f.replace('imgs', '').replace('/','');
-							}
-							else if (f[f.length - 1] !== '/') {
-								filePath = `${location.href}${currentFolder}${f}`;
-							}
-						}
-			  
-						return filePath;
-					})
-					.filter(f => f); // Remove empty lines
-		  
-				const imagesContainer = document.getElementById('glaciers');
-		  
-				filePaths.map(f => { // Create and put images to the DOM
-					const img = document.createElement('IMG');
-					img.setAttribute('class', "materialboxed");
-					img.setAttribute('data-caption', "Has anybody ever told you how cute your generic, nondescript features are?");
-					img.setAttribute('width', "350");
-					img.setAttribute('artist', artistName);
-					img.src = f;
-					imagesContainer.appendChild(img);
-				});
-			}
+				if (f) {
+					if (f[0] === '.') {
+						currentFolder = f.replace('.', '').replace(':', '/');
+						artistName = f.replace('imgs', '').replace('/','');
+					}
+					else if (f[f.length - 1] !== '/') {
+						filePath = `${location.href}${currentFolder}${f}`;
+					}
+				}
+	  
+				return filePath;
+			})
+			.filter(f => f); // Remove empty lines
+	  
+			const imagesContainer = document.getElementById('glaciers');
+	  
+			filePaths.map(f => { // Create and put images to the DOM
+				const img = document.createElement('IMG');
+				img.setAttribute('class', "materialboxed");
+				img.setAttribute('data-caption', "Has anybody ever told you how cute your generic, nondescript features are?");
+				img.setAttribute('width', "350");
+				img.setAttribute('artist', artistName);
+				img.src = f;
+				imagesContainer.appendChild(img);
+			});
 		}
 	};
 
 	xhttp.open("GET", "images.txt", true);
 	xhttp.send();
-	loadAmt++;
+}
+
+function populateDescriptions() {
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			const fileList = this.responseText.split('\n'); // Split by lines
+			let message = '';
+			let artistName = '';
+			
+			const filePaths = fileList
+			.map(f => { // Build correct path for each file
+
+				if (f) {
+					if (f[0] === '.') {
+						var strs = f.split('{');
+						message = strs[1];
+						artistName = strs[0];
+					}
+				}
+	  
+				return message;
+			})
+			.filter(f => f); // Remove empty lines
+	  
+			const images = document.querySelectorAll('[artist="' + artistName + '"]');
+	  
+			images.forEach(function (arrayItem) {
+				arrayItem.setAttribute('data-caption', message);
+			});
+		}
+	};
+
 	xhttp.open("GET", "descs.txt", true);
 	xhttp.send();
 }
 
 loadImages();
+populateDescriptions();
